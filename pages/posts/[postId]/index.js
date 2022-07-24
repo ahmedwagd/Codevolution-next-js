d85e2361ec8773eef45f955a6019272526d8e746
ahmedwagd <ahmedwa122@gmail.com>
2022-07-24 19:18:39 +0200

    getStaticPaths > fallback: true

--- pages/posts/[postId]/index.js
+++ pages/posts/[postId]/index.js
@@ -1,5 +1,10 @@
+import { useRouter } from 'next/router';
 
 function PostDetails({ post }) {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <h1>Loading....</h1>
+  }
   return (
     <div style={{ textAlign: 'center', padding: '100px 50px 0px', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: '150px', flexDirection: 'column' }}>
       <h1>{post.title}</h1>
@@ -10,17 +15,6 @@ function PostDetails({ post }) {
   )
 }
 export default PostDetails;
-
-export async function getStaticProps(context) {
-  const { params } = context
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
-  const data = await response.json()
-  return {
-    props: {
-      post: data
-    }
-  }
-}
 
 export async function getStaticPaths() {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
@@ -36,6 +30,23 @@ export async function getStaticPaths() {
     paths: [
       ...paths
     ],
-    fallback: false
+    fallback: true
+  }
+}
+
+export async function getStaticProps(context) {
+  const { params } = context
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
+  const data = await response.json()
+  if (!data.id) {
+    return {
+      notFound: true
+    }
+  }
+  console.log(`Generating page for /post/${params.postId}`);
+  return {
+    props: {
+      post: data
+    }
   }
 }
--- pages/users/[userId]/index.js
+++ pages/users/[userId]/index.js
@@ -14,6 +14,7 @@ function UserDetails({ user }) {
 }
 export default UserDetails;
 
+
 export async function getStaticProps(context) {
   const { params } = context
   const response = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`);
@@ -24,7 +25,25 @@ export async function getStaticProps(context) {
     }
   }
 }
-
+/*
+ * * gestStaticPaths > fallback : true 
+ * 1- The paths returned from gestStaticPaths will be rerenderd to HTML at build time by gestStaticProps.
+ * 2- The paths that have not been generated at build time will not  result in a 404 page. Instead, Next.js will serve a "fallback" version on the page on the first request to such a path.
+ * 3- In the background, Next.js will statically generated the requested path HTML and JSON. This includes running gestStaticProps.
+ * 4- When that's done, the browser recevies the JSON for the generated path. This will be used to automatically render the page with the required props.
+ *       From the user's prespective, the page will be sapped from the callback page to full page.
+ *  5- At the same time, Next.js keeps track of the new list of pre-rendered pages. Subsequent request to the same path will serve the generated page,
+ *        just like other pages pre-rendered at build time.
+ * * When?
+ * The true value is most suitable if your app has a very large number of static pages that depend on data
+ * A large e-commerce site.
+ * You want all the product pages to be pre-rendered but if you have thousand products, builds can take a really long time.
+ * You may statically generate a small subset of products that are popular and use fallback : true for the rest.
+ * When someone requests a page that's not generated yet, the user will see the page with loading indicator.
+ * Shortly after, getStaticProps finishes and the page will be rendered with the requested data.
+ *    From then onwards, everyone  who requests the same page  will get the statically pre-rendered page.
+ * This ensures that the users always have a fast experience while  preserving fast build and benefits of Static Generation. 
+ */
 export async function getStaticPaths() {
   const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
   const data = await response.json();
